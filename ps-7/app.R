1090acf17d820e8f9815fd2d2f7226acae34095e
Steven Espinoza <steven_esp12@Stevens-MacBook-Air-3.local>
2018-11-21 01:45:44 -0500

    getting shiny app to work, now finishing labels

--- ps-7/app.R
+++ ps-7/app.R
@@ -1,5 +1,8 @@
 library(shiny)
 
+df <- read.csv('merged_thing.csv') %>% 
+  select(-X)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
@@ -11,13 +14,13 @@ ui <- fluidPage(
       sidebarPanel(
          selectInput(inputId = "question",
                      label = "Select a Polling Topic:",
-                     choices = c('NAFTA', 'Tariff', 'Tax Reform', 'Trump Economy', 'Single-Payer'),
-                     selected = 'Single-Payer')
+                     choices = c('NAFTA', 'Tariffs', 'Tax Reform', 'Trump Economy', 'Single Payer Healthcare'),
+                     selected = 'NAFTA')
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotlyOutput("distPlot")
       )
    )
 )
@@ -25,13 +28,61 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$distPlot <- renderPlotly({
+     if(input$question == 'NAFTA'){
+     df %>% 
+       select(v_9, error, nafta) %>% 
+       mutate('newname' = paste(str_to_upper(substr(v_9, 1, 2)), "-", substr(v_9, 3, 4), sep='')) %>%
+       filter(!is.na(nafta)) %>% 
+       plot_ly(x = ~nafta, y = ~error, 
+               text = ~newname,
+               color = ~newname) %>% 
+       layout(xaxis = list(title = '% Support/Agree with Statement'), 
+              yaxis = list(title = 'Error in predicting Democratic Advantage'))}
+     
+     else if(input$question == 'Tariffs'){
+       df %>% 
+         select(v_9, error, tariff) %>% 
+         mutate('newname' = paste(str_to_upper(substr(v_9, 1, 2)), "-", substr(v_9, 3, 4), sep='')) %>%
+         filter(!is.na(tariff)) %>% 
+         plot_ly(x = ~tariff, y = ~error, 
+                 text = ~newname,
+                 color = ~newname) %>% 
+         layout(xaxis = list(title = '% Support/Agree with Statement'), 
+                yaxis = list(title = 'Error in predicting Democratic Advantage'))}
+     
+     else if(input$question == 'Tax Reform'){
+       df %>% 
+         select(v_9, error, taxreform) %>% 
+         mutate('newname' = paste(str_to_upper(substr(v_9, 1, 2)), "-", substr(v_9, 3, 4), sep='')) %>%
+         filter(!is.na(taxreform)) %>% 
+         plot_ly(x = ~taxreform, y = ~error, 
+                 text = ~newname,
+                 color = ~newname) %>% 
+         layout(xaxis = list(title = '% Support/Agree with Statement'), 
+                yaxis = list(title = 'Error in predicting Democratic Advantage'))}
+     
+     else if(input$question == 'Trump Economy'){
+       df %>% 
+         select(v_9, error, trumpecon) %>% 
+         mutate('newname' = paste(str_to_upper(substr(v_9, 1, 2)), "-", substr(v_9, 3, 4), sep='')) %>%
+         filter(!is.na(trumpecon)) %>% 
+         plot_ly(x = ~trumpecon, y = ~error, 
+                 text = ~newname,
+                 color = ~newname) %>% 
+         layout(xaxis = list(title = '% Support/Agree with Statement'), 
+                yaxis = list(title = 'Error in predicting Democratic Advantage'))}
+     
+     else if(input$question == 'Single Payer Healthcare'){
+       df %>% 
+         select(v_9, error, singlepay) %>% 
+         mutate('newname' = paste(str_to_upper(substr(v_9, 1, 2)), "-", substr(v_9, 3, 4), sep='')) %>%
+         filter(!is.na(singlepay)) %>% 
+         plot_ly(x = ~singlepay, y = ~error, 
+                 text = ~newname,
+                 color = ~newname) %>% 
+         layout(xaxis = list(title = '% Support/Agree with Statement'), 
+                yaxis = list(title = 'Error in predicting Democratic Advantage'))}
    })
 }
 
